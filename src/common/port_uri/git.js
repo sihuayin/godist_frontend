2f48f5b9ea71e458a711b1e391120fce006f6ff2
司华印(10034744) <sihy-a@glodon.com>
2021-02-06 15:02:48 +0800

    first

---
+++ src/common/port_uri/git.js
@@ -0,0 +1,12 @@
+//获取分支
+exports.branch = "/api/get/git/branch"
+//获取tag
+exports.getTag = "/api/get/git/tag"
+//获取提交
+exports.commit = "/api/get/git/commit"
+
+exports.gitlog = "/api/get/git/gitlog"
+exports.gitpull = "/api/get/git/gitpull"
+
+//获取分支
+exports.jenkinsBranch = "/api/get/jenkins/commit"
